f2181ab42ace45c3fbf5e0376c90a314bf27bfb1
Angelos <kounavisangelo@gmail.com>
2022-11-16 20:47:34 -0500

    lab-08

--- server.js
+++ server.js
@@ -1,19 +1,18 @@
 'use strict';
-
-const { response } = require('express');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
 // REQUIRE
 // in our servers, we have to use 'require' instead of 'import'
 // Here we will list the requirments for a server
-const express = require('express');
-let data = require('./data/weather.json');
+// const express = require('express');
+let weather = require('./data/weather.json');
 
 
 // we need to bring in our .env file, so we'll use this after we have installed
 // `npm i dotenv`
-require('dotenv').config();
 
 // we must include CORS if we want to share resources over the web
-const cors = require('cors');
 
 // USE
 // once we have required something, we have to use it
@@ -26,7 +25,7 @@ const app = express();
 app.use(cors());
 
 // define the PORT and validate that our .env is working
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 // If we see our server running on 3002, that means theere's a problem with our .env file or how we are importing it.
 
@@ -36,23 +35,29 @@ const PORT = process.env.PORT || 3002;
 // create a basic default route
 // app.get() correlates to axios.get()
 // app.get() takes in a parament or a URL in quotes, and callback function
-app.get('/', (request, response) => {
-  response.send('Hello, from our server');
-});
+// app.get('/', (request, response) => {
+//   response.send('Hello, from our server');
+// });
 
-app.get('/sayHello', (request, response) => {
-  console.log(request.query.name);
-  let lastName = request.query.lastName
-  response.send(`Hi ${request.query.name} ${lastName}`);
-});
+// app.get('/sayHello', (request, response) => {
+//   console.log(request.query.name);
+//   let lastName = request.query.lastName;
+//   response.send(`Hi ${request.query.name} ${lastName}`);
+// });
 
-app.get('/weather', (request, response, next) => {
+app.get('/weather', handleWeather);
+app.use('*', (request, response) => response.status(404).send('No page found'));
+
+function handleWeather (request, response) {
+  let { searchQuery } = request.query;
+  const city = weather.find(city => city.city_name.toLowerCase() === searchQuery.toLowerCase());
+
   try {
     // http://localhost:3001/weather?city=Seattle
-    let cityInput = request.query.city;
-    let selectedCity = data.find(eachCityObj=> eachCityObj.city_name === cityInput);
-    console.log(selectedCity);
-    let weatherArr = selectedCity.data.map(day => new Forecast(day));
+    // let cityInput = request.query.city;
+    // let selectedCity = data.find(eachCityObj=> eachCityObj.city_name === cityInput);
+    // console.log(selectedCity);
+    const weatherArr = city.data.map(day => new Forecast(day));
     response.status(200).send(weatherArr);
     console.log(weatherArr);
 
@@ -60,13 +65,21 @@ app.get('/weather', (request, response, next) => {
     // create a new instance of the Error object that lives in Express
     errorHandler(error, response);
   }
-});
+}
+
+function Forecast(day) {
+  this.day = day.valid_date;
+  this.description = day.weather.description;
+}
+
+
+
 
 // '*' wild card
 // this will run for any route not defined above
-app.get('*', (request, response) => {
-  response.send('That route does not exist');
-});
+// app.get('*', (request, response) => {
+//   response.send('That route does not exist');
+// });
 
 // ERRORS
 // handle any errors
@@ -75,12 +88,12 @@ function errorHandler(error, response) {
 }
 
 // CLASSES
-class Forecast {
-  constructor(day) {
-    this.date = day.valid_date;
-    this.description =day.weather.description;
-  }
-}
+// class Forecast {
+//   constructor(day) {
+//     this.date = day.valid_date;
+//     this.description =day.weather.description;
+//   }
+// }
 
 
 // LISTEN
